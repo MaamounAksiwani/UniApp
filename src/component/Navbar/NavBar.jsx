06423dff31a28f4ca3714f99a4c361fa8a17915c
Maamoun <maamoun.malkiswani@gmail.com>
2024-02-03 02:19:14 +0300

    edit navbar

--- src/component/Navbar/NavBar.jsx
+++ src/component/Navbar/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Container from "@mui/material/Container";
 import { Link, useLocation } from 'react-router-dom';
 import './NavBar.css';
+import MenuIcon from '@mui/icons-material/Menu';
 
 
 const Navbar = () => {
@@ -36,10 +37,7 @@ const Navbar = () => {
 
     return (
         <body className='bodyNavBar'>
-
-
             <div style={navbarStyle}>
-
                 <div style={{
                     background: !isScrolled && (currentPath !== '/study-destinations/study-in-germany/'
                         && currentPath !== '/study-destinations/' && currentPath !== '/about-us/' && currentPath !== '/bachelors-degrees/' && currentPath !== '/online-degrees/') ? 'transparent' : '#FFF', transition: 'background 0.3s ease'
@@ -50,10 +48,14 @@ const Navbar = () => {
                                 <img src='https://static.udrus.com/assets/image/uniapp-logo.png' alt='logo not found' />
                             </Link>
                             <div className={`pages ${showPages ? 'show' : ''}`}>
-                                {/* <a href="#">Find Programs</a> */}
-                                <Link style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} to='/students/'>Students</Link>
-                                <Link style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} to="/study-agents/">Study Agents</Link>
-                                <Link style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} to='/schools/' >School</Link>
+
+                                <span  style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }}  onClick={() => { window.location.href = 'https://uni-app.com/search' }}>Find Programs </span>
+                                <Link className='navBarLink' style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} to='/students/'>Students</Link>
+                                <Link className='navBarLink' style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} to="/study-agents/">Study Agents</Link>
+                                <Link className='navBarLink' style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} to='/schools/' >School</Link>
+                                <MenuIcon onClick={handleToggleMenu} style={{ marginTop: '7px' , cursor:'pointer' ,color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} />
+                                <span onClick={()=>{window.location.href = 'https://my.uni-app.com/sign-in?redirectURL=%2Fhome'}} className='btn'  style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} >Login </span>
+                                <span  onClick={()=>{window.location.href = 'https://my.uni-app.com/sign-up'}} className='btn'   style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} >Create Account </span>
 
                                 {/* <div  className="burger-menu-container">
                                 <div className={`burger-menu ${isOpen ? 'open' : ''}`} onClick={handleToggleMenu}>
@@ -70,11 +72,11 @@ const Navbar = () => {
                                 )}
                             </div> */}
                             </div>
-                            <div className="burger" onClick={togglePages}>
+                            {/* <div className="burger" onClick={togglePages}>
                                 <div className="line"></div>
                                 <div className="line"></div>
                                 <div className="line"></div>
-                            </div>
+                            </div> */}
                         </nav>
                     </Container>
                 </div>
