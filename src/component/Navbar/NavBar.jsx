351b25fb733f047f855acaea76012f190bf7dca3
Maamoun <maamoun.malkiswani@gmail.com>
2024-02-03 03:31:28 +0300

    side bar

--- src/component/Navbar/NavBar.jsx
+++ src/component/Navbar/NavBar.jsx
@@ -1,20 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import Container from "@mui/material/Container";
 import { Link, useLocation } from 'react-router-dom';
+
+import CloseIcon from '@mui/icons-material/Close';
 import './NavBar.css';
 import MenuIcon from '@mui/icons-material/Menu';
-
 
 const Navbar = () => {
     const location = useLocation();
     const currentPath = location.pathname;
-    const [showPages, setShowPages] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
     const [isScrolled, setIsScrolled] = useState(false);
-
-    const togglePages = () => setShowPages(!showPages);
     const handleToggleMenu = () => setIsOpen(!isOpen);
-    const handleMouseEnterDropdown = () => setIsOpen(true);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -35,52 +32,76 @@ const Navbar = () => {
         zIndex: 10000000,
     };
 
+    const sidebarStyle = {
+        position: 'fixed',
+        top: 0,
+        right: isOpen ? 0 : '-100%', // Show/hide the sidebar
+        height: '100%',
+        width: '250px',
+        backgroundColor: '#fff',
+        boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+        transition: 'right 0.3s ease',
+    };
+
     return (
         <body className='bodyNavBar'>
             <div style={navbarStyle}>
                 <div style={{
                     background: !isScrolled && (currentPath !== '/study-destinations/study-in-germany/'
-                        && currentPath !== '/study-destinations/' && currentPath !== '/about-us/' && currentPath !== '/bachelors-degrees/' && currentPath !== '/online-degrees/') ? 'transparent' : '#FFF', transition: 'background 0.3s ease'
+                        && currentPath !== '/study-destinations/' && currentPath !== '/about-us/' && currentPath !== '/bachelors-degrees/'
+                        && currentPath !== '/online-degrees/') ? 'transparent' : '#FFF', transition: 'background 0.3s ease'
                 }}>
                     <Container maxWidth="lg">
                         <nav className="navbar">
                             <Link to='/' className="logo">
                                 <img src='https://static.udrus.com/assets/image/uniapp-logo.png' alt='logo not found' />
                             </Link>
-                            <div className={`pages ${showPages ? 'show' : ''}`}>
-
-                                <span  style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }}  onClick={() => { window.location.href = 'https://uni-app.com/search' }}>Find Programs </span>
-                                <Link className='navBarLink' style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} to='/students/'>Students</Link>
-                                <Link className='navBarLink' style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} to="/study-agents/">Study Agents</Link>
-                                <Link className='navBarLink' style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} to='/schools/' >School</Link>
-                                <MenuIcon onClick={handleToggleMenu} style={{ marginTop: '7px' , cursor:'pointer' ,color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} />
-                                <span onClick={()=>{window.location.href = 'https://my.uni-app.com/sign-in?redirectURL=%2Fhome'}} className='btn'  style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} >Login </span>
-                                <span  onClick={()=>{window.location.href = 'https://my.uni-app.com/sign-up'}} className='btn'   style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} >Create Account </span>
-
-                                {/* <div  className="burger-menu-container">
-                                <div className={`burger-menu ${isOpen ? 'open' : ''}`} onClick={handleToggleMenu}>
-                                    <div className="bar"></div>
-                                    <div className="bar"></div>
-                                    <div className="bar"></div>
-                                </div>
-                                {isOpen && (
-                                    <div className="dropdown-menu" onMouseEnter={handleMouseEnterDropdown}>
-                                        <div>Menu Item 1</div>
-                                        <div>Menu Item 2</div>
-                                        <div>Menu Item 3</div>
-                                    </div>
-                                )}
-                            </div> */}
+                            <div className={`pages`}>
+                                <span style={{ color: !isScrolled && currentPath === '/' ? '#fff' : '#000' }} onClick={() => { window.location.href = 'https://uni-app.com/search' }}>Find Programs </span>
+                                <Link className='navBarLink' style={{ color: !isScrolled && currentPath === '/' ? '#fff' : '#000' }} to='/students/'>Students</Link>
+                                <Link className='navBarLink' style={{ color: !isScrolled && currentPath === '/' ? '#fff' : '#000' }} to="/study-agents/">Study Agents</Link>
+                                <Link className='navBarLink' style={{ color: !isScrolled && currentPath === '/' ? '#fff' : '#000' }} to='/schools/' >School</Link>
+                                <span onClick={() => { window.location.href = 'https://my.uni-app.com/sign-in?redirectURL=%2Fhome' }} className='btn' style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} >Login </span>
+                                <span onClick={() => { window.location.href = 'https://my.uni-app.com/sign-up' }} className='btn' style={{ color: !isScrolled && currentPath == '/' ? '#fff' : '#000' }} >Create Account </span>
                             </div>
-                            {/* <div className="burger" onClick={togglePages}>
-                                <div className="line"></div>
-                                <div className="line"></div>
-                                <div className="line"></div>
-                            </div> */}
                         </nav>
+
+                        <div className='smallMedia'>
+                            <div>
+                                <Link to='/' className="logo">
+                                    <img src='https://static.udrus.com/assets/image/uniapp-logo.png' alt='logo not found' />
+                                </Link>
+                                <MenuIcon onClick={handleToggleMenu} style={{ fontSize: '25px', cursor: 'pointer', color: !isScrolled && currentPath === '/' ? '#fff' : '#000' }} />
+                            </div>
+                        </div>
+
+                        {/* Sidebar */}
+                        <div style={sidebarStyle}>
+                            <div className='sideBar'>
+
+                                <Link to='/' className="logo">
+                                    <img src='https://static.udrus.com/assets/image/uniapp-logo.png' alt='logo not found' />
+                                </Link>
+                                <CloseIcon style={{ fontSize: '30px' }} />
+                            </div>
+
+                            <div className='menuSidebar'>
+
+                            <Link to='/students/' className='menuNavBarLink' onClick={handleToggleMenu}>Students</Link>
+                            <hr style={{ width: '90%', borderTop: '1px solid #fff' }} />
+
+                            <Link to='/study-agents/' className='menuNavBarLink' onClick={handleToggleMenu}>Study Agents</Link>
+                            <hr style={{ width: '90%', borderTop: '1px solid #fff'}} />
+
+                            <Link to='/schools/' className='menuNavBarLink' onClick={handleToggleMenu}>School</Link>
+                            <hr style={{ width: '90%', borderTop: '1px solid #fff' }} />
+
+                            </div>
+                         
+                        </div>
                     </Container>
                 </div>
-            </div >
+            </div>
         </body>
     );
 };
