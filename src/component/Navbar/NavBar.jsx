cb651a75a9b73dc5fb57b774d809e7747e0be544
Maamoun <maamoun.malkiswani@gmail.com>
2024-02-08 19:19:15 +0300

    fix

--- src/component/Navbar/NavBar.jsx
+++ src/component/Navbar/NavBar.jsx
@@ -116,7 +116,7 @@ const Navbar = () => {
                                             <p onClick={() => { navigate('/study-medicine-in-europe/') }}>Study Medicine</p>
                                             <p onClick={() => { navigate('/study-destinations/') }}>Study Destinations</p>
                                             <p onClick={() => { navigate('/student-application-guide/') }}>Student Application Guide</p>
-                                            <p>Articles</p>
+                                            {/* <p>Articles</p> */}
                                             <p onClick={() => { navigate('/about-us/') }}>About us</p>
                                         </div>
                                     )}
